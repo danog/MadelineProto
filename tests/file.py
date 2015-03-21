bed593c9242006398a747ea9fd2fdc2eb1cdff24
chidea <sbw228@gmail.com>
2015-03-22 01:44:43 +0900

    test fix

--- tests/file.py
+++ tests/file.py
@@ -2,7 +2,7 @@ from classes.file import File
 from os.path import exists
 
 f = File('text.txt')
-assert f.write_bytes(b'testing bytes i/o')
+assert f.write_bytes(b'testing bytes i/o'), 17
 assert f.read_bytes(), b'testing bytes i/o'
 f.open() # does it open any text editor on your system?
 f.remove()
