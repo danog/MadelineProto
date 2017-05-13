d9dfd83704620153b3c1baa1a4e7c0cd5522fd16
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-05-13 10:53:22 +0100

    More fixes

--- src/danog/MadelineProto/MTProtoTools/ResponseHandler.php
+++ src/danog/MadelineProto/MTProtoTools/ResponseHandler.php
@@ -79,7 +79,7 @@ trait ResponseHandler
         foreach ($this->datacenter->sockets[$datacenter]->new_incoming as $current_msg_id) {
             $unset = false;
             \danog\MadelineProto\Logger::log(['Received '.$this->datacenter->sockets[$datacenter]->incoming_messages[$current_msg_id]['content']['_'].'.'], \danog\MadelineProto\Logger::VERBOSE);
-            if (\danog\MadelineProto\Logger::$has_thread && method_exists('\Thread', 'getCurrentThread') && is_object(\Thread::getCurrentThread())) {
+            if (\danog\MadelineProto\Logger::$has_thread && is_object(\Thread::getCurrentThread())) {
                 if (!$this->synchronized(function ($zis, $datacenter, $current_msg_id) {
                     if (isset($this->datacenter->sockets[$datacenter]->incoming_messages[$current_msg_id]['handling'])) {
                         return false;
