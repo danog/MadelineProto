1dff27e48778031b1f132e2c1ec7e7e32a927672
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-08-06 22:54:00 +0000

    Apply fixes from StyleCI

--- songs.php
+++ songs.php
@@ -1,10 +1,10 @@
 <?php
 
 $songs = [
-    'Aronchupa - Little Swing'     => 'input.raw',
-    'Parov Stelar - Booty Swing'   => 'inputa.raw',
-    'Parov Stelar - All night'     => 'inpute.raw',
-    'Caravan Palace - Lone Digger' => 'inputb.raw',
+    'Aronchupa - Little Swing'         => 'input.raw',
+    'Parov Stelar - Booty Swing'       => 'inputa.raw',
+    'Parov Stelar - All night'         => 'inpute.raw',
+    'Caravan Palace - Lone Digger'     => 'inputb.raw',
     'Postmodern Jukebox - Thrift Shop' => 'inputd.raw',
 ];
 for ($x = 0; $x < count($songs); $x++) {
