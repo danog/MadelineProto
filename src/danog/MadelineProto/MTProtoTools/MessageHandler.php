71f392e2b954395a52b0615b50eb079f00cf048d
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-09-26 15:08:25 +0200

    Added userbot examples, improved performance of gzip compression

--- src/danog/MadelineProto/MTProtoTools/MessageHandler.php
+++ src/danog/MadelineProto/MTProtoTools/MessageHandler.php
@@ -119,7 +119,6 @@ trait MessageHandler
         $this->datacenter->sockets[$datacenter]->incoming_messages[$message_id]['response'] = -1;
         $this->datacenter->sockets[$datacenter]->new_incoming[$message_id] = $message_id;
         $this->last_recv = time();
-
         return true;
     }
 }
