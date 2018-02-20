cc635bcb27136298fc80d3bb4edd95431adfdbb5
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-02-20 12:51:02 +0000

    PHP 5 fixes

--- src/BigIntegor.php
+++ src/BigIntegor.php
@@ -2,7 +2,7 @@
 
 namespace phpseclib\Math;
 
-if (PHP_MAJOR_VERSION < 7 && !(class_exists('Phar') && Phar::running())) {
+if (PHP_MAJOR_VERSION < 7 && !(class_exists('\Phar') && \Phar::running())) {
     throw new Exception('MadelineProto requires php 7 to run');
 }
 
