d019befcb3da74884f5f28b4c0133eac8b894f4e
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-03-03 14:43:14 +0000

    Apply fixes from StyleCI

--- src/danog/MadelineProto/MTProtoTools/PeerHandler.php
+++ src/danog/MadelineProto/MTProtoTools/PeerHandler.php
@@ -562,6 +562,7 @@ trait PeerHandler
             $res = $this->method_call('contacts.resolveUsername', ['username' => str_replace('@', '', $username)], ['datacenter' => $this->datacenter->curdc]);
         } catch (\danog\MadelineProto\RPCErrorException $e) {
             \danog\MadelineProto\Logger::log('Username resolution failed with error '.$e->getMessage(), \danog\MadelineProto\Logger::ERROR);
+
             return false;
         }
         if ($res['_'] === 'contacts.resolvedPeer') {
