c2185176738a52a5323d029899ce9d1e6829032c
Daniil Gentili <daniil@daniil.it>
2018-03-04 14:37:45 +0100

    HTTP fixes

--- src/danog/MadelineProto/MTProtoTools/CallHandler.php
+++ src/danog/MadelineProto/MTProtoTools/CallHandler.php
@@ -185,7 +185,7 @@ trait CallHandler
                     } catch (\danog\MadelineProto\NothingInTheSocketException $e) {
                         $last_error = 'Nothing in the socket';
                         \danog\MadelineProto\Logger::log('An error getting response of method '.$method.': '.$e->getMessage().' in '.basename($e->getFile(), '.php').' on line '.$e->getLine().'. Retrying...', \danog\MadelineProto\Logger::WARNING);
-                        if ($res_count > 3 && $last_recv === $this->datacenter->sockets[$aargs['datacenter']]->last_recv) {
+                        if ($last_recv === $this->datacenter->sockets[$aargs['datacenter']]->last_recv) {
                             $this->close_and_reopen($aargs['datacenter']);
                         }
                         continue; //2;
