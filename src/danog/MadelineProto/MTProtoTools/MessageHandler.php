11a82740299cfe11260fa68e50a85d6be06dbf25
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-03-06 19:03:16 +0000

    Apply fixes from StyleCI

--- src/danog/MadelineProto/MTProtoTools/MessageHandler.php
+++ src/danog/MadelineProto/MTProtoTools/MessageHandler.php
@@ -85,7 +85,7 @@ trait MessageHandler
             \danog\MadelineProto\Logger::log('Trying to read from closed socket, sending initial ping');
             if ($this->is_http($datacenter)) {
                 $this->method_call('http_wait', ['max_wait' => 500, 'wait_after' => 150, 'max_delay' => 500], ['datacenter' => $datacenter]);
-            } else if (isset($this->datacenter->sockets[$datacenter]->temp_auth_key['connection_inited']) && $this->datacenter->sockets[$datacenter]->temp_auth_key['connection_inited']) {
+            } elseif (isset($this->datacenter->sockets[$datacenter]->temp_auth_key['connection_inited']) && $this->datacenter->sockets[$datacenter]->temp_auth_key['connection_inited']) {
                 $this->method_call('ping', ['ping_id' => 0], ['datacenter' => $datacenter]);
             }
         }
