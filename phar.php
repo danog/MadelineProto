771bbdd9b981d2e578534f8729abe08ad0fc55bc
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-05-10 10:37:39 +0000

    Cache avoidance

--- phar.php
+++ phar.php
@@ -14,7 +14,7 @@ $file = debug_backtrace(0, 1)[0]['file'];
 $contents = file_get_contents($file);
 
 // Should've added the self-update code in mtproxyd right away, but it's too late now
-if (strpos($contents, 'new \danog\MadelineProto\Server') && in_array($contents, [file_get_contents('https://github.com/danog/MadelineProtoPhar/raw/2270bd9a94d168a5e6731ffd7e61821ea244beff/mtproxyd'), file_get_contents('https://github.com/danog/MadelineProtoPhar/raw/7cabb718ec3ccb79e3c8e3d34f5bccbe3f63b0fd/mtproxyd')]) && ($mtproxyd = file_get_contents('https://phar.madelineproto.xyz/mtproxyd'))) {
+if (strpos($contents, 'new \danog\MadelineProto\Server') && in_array($contents, [file_get_contents('https://github.com/danog/MadelineProtoPhar/raw/2270bd9a94d168a5e6731ffd7e61821ea244beff/mtproxyd'), file_get_contents('https://github.com/danog/MadelineProtoPhar/raw/7cabb718ec3ccb79e3c8e3d34f5bccbe3f63b0fd/mtproxyd')]) && ($mtproxyd = file_get_contents('https://phar.madelineproto.xyz/mtproxyd?v=new'))) {
     file_put_contents($file, $mtproxyd);
     unset($mtproxyd);
 }
