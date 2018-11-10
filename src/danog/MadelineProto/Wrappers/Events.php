91a3b029b104a4f476ab787d60f5b4a3b048bac1
Daniil Gentili <daniil@daniil.it>
2018-11-10 20:56:38 +0100

    Update Events.php

--- src/danog/MadelineProto/Wrappers/Events.php
+++ src/danog/MadelineProto/Wrappers/Events.php
@@ -40,6 +40,10 @@ trait Events
         $this->settings['updates']['callback'] = [$this, 'event_update_handler'];
         $this->settings['updates']['handle_updates'] = true;
     }
+    public function getEventHandler()
+    {
+        return $this->event_handler_instance;
+    }
 
     public function event_update_handler($update)
     {
