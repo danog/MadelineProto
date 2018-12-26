50fe3695da2c5471ae39fe36506383d87e84a50d
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-12-26 11:13:10 +0000

    Avoid updating if using MTProxy

--- phar.php
+++ phar.php
@@ -16,6 +16,7 @@ function ___install_madeline() {
 
         if (strpos($contents, 'new \danog\MadelineProto\Server') && in_array($contents, [file_get_contents('https://github.com/danog/MadelineProtoPhar/raw/2270bd9a94d168a5e6731ffd7e61821ea244beff/mtproxyd'), file_get_contents('https://github.com/danog/MadelineProtoPhar/raw/7cabb718ec3ccb79e3c8e3d34f5bccbe3f63b0fd/mtproxyd')]) && ($mtproxyd = file_get_contents('https://phar.madelineproto.xyz/mtproxyd?v=new'))) {
             file_put_contents($file, $mtproxyd);
+            return;
         }
     }
 
