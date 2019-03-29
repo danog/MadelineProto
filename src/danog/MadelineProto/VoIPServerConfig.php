b306c98e4b3fd06fb1facdaaa1a3eb1b8883d092
Daniil Gentili <daniil.gentili.dg@gmail.com>
2019-03-29 23:05:29 +0000

    Fix

--- src/danog/MadelineProto/VoIPServerConfig.php
+++ src/danog/MadelineProto/VoIPServerConfig.php
@@ -89,4 +89,9 @@ if (class_exists('\\danog\\MadelineProto\\VoIPServerConfigInternal')) {
             return array_merge(self::$_configDefault, self::$_config);
         }
     }
+} else {
+    class VoIPServerConfig
+    {
+        public static function update(array $config) {}
+    }
 }
