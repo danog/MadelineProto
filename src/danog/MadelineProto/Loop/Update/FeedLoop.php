6014b1119ec111f438012e8901cb735874d74a61
Mattia <36816061+MattiaBLX@users.noreply.github.com>
2019-06-22 20:35:06 +0200

    undefined index pts_count fix

--- src/danog/MadelineProto/Loop/Update/FeedLoop.php
+++ src/danog/MadelineProto/Loop/Update/FeedLoop.php
@@ -101,6 +101,7 @@ class FeedLoop extends ResumableSignalLoop
                 continue;
             }
             if (isset($update['pts'])) {
+                $update['pts_count'] = !isset($update['pts_count']) ?: 1;
                 $logger = function ($msg) use ($update) {
                     $pts_count = $update['pts_count'];
                     $double = isset($update['message']['id']) ? $update['message']['id'] * 2 : '-';
