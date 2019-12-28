491b9956f85a3a447436c18c4adb8392761aa9ed
Daniil Gentili <daniil@daniil.it>
2019-12-28 01:02:50 +0100

    Build tags

--- tests/waitPackagist.php
+++ tests/waitPackagist.php
@@ -1,10 +1,13 @@
 #!/usr/bin/env php
 <?php
 
+$tag = \getenv('TRAVIS_TAG');
+$branch = $tag ? $tag : "dev-".\getenv("TRAVIS_BRANCH");
+
 while (true) {
     $json = \json_decode(\file_get_contents('https://repo.packagist.org/p/danog/madelineproto.json'), true);
-    if ($json["packages"]["danog/madelineproto"]["dev-".\getenv("TRAVIS_BRANCH")]["source"]["reference"] === \getenv('TRAVIS_COMMIT')) {
+    \sleep(1);
+    if ($json["packages"]["danog/madelineproto"][$branch]["source"]["reference"] === \getenv('TRAVIS_COMMIT')) {
         return;
     }
-    \sleep(1);
 }
