398970e3061b3fe8d115f87a86a3c129f6e90e71
Daniil Gentili <daniil@daniil.it>
2021-04-08 21:08:41 +0200

    Change visibility

--- src/danog/MadelineProto/Db/DbPropertiesTrait.php
+++ src/danog/MadelineProto/Db/DbPropertiesTrait.php
@@ -16,7 +16,7 @@ use danog\MadelineProto\Tools;
  */
 trait DbPropertiesTrait
 {
-    private ?string $tmpDbPrefix = null;
+    public ?string $tmpDbPrefix = null;
 
     /**
      * Initialize database instance.
