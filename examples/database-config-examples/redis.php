8a4a6b1ea5768debb3026e60fc9f78cbc532be87
wsudo <mahdiomati@gmail.com>
2021-12-09 17:59:49 +0330

    add redis database config example file

---
+++ examples/database-config-examples/redis.php
@@ -0,0 +1,54 @@
+<?php
+use danog\MadelineProto\Settings;
+use danog\MadelineProto\API;
+
+/* 
+    * load MadelineProto in Your Project
+*/
+
+
+if (\file_exists('vendor/autoload.php')) {
+    include 'vendor/autoload.php';
+} else {
+    if (!\file_exists('madeline.php')) {
+        \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+    }
+    include 'madeline.php';
+}
+
+/* 
+    * create default mysql object
+*/
+$database = new \danog\MadelineProto\Settings\Database\Redis;
+
+/* 
+    config and set auth information
+*/
+
+$database->setUri('127.0.0.1:6379');
+$database->setDatabase(0); // redis database number
+$database->setPassword('wsudo');
+
+/* 
+    create default settings object
+*/
+
+$settings = new Settings;
+
+/* 
+    set and merge database settings to main settings
+*/
+
+$settings->setDb($database);
+
+
+/* 
+    create madeline proto session
+*/
+
+$MadelineProto = new API('madeline.session' , $settings);
+
+/* 
+    start madeline proto workers
+*/
+$MadelineProto->start();
