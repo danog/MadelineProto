7b5ed47d8f2c79513c3489af070b706337324e0d
Lukas Pierce <kosha.industry@gmail.com>
2021-12-13 06:42:02 +0600

    remove coment

--- src/danog/MadelineProto/MTProtoTools/UpdateHandler.php
+++ src/danog/MadelineProto/MTProtoTools/UpdateHandler.php
@@ -335,7 +335,6 @@ trait UpdateHandler
                     if (!isset($this->calls[$update['phone_call']['id']])) {
                         return;
                     }
-                    // just test comment
                     return $this->calls[$update['phone_call']['id']]->discard($update['phone_call']['reason'] ?? ['_' => 'phoneCallDiscardReasonDisconnect'], [], $update['phone_call']['need_debug'] ?? false);
             }
         }
