12bb67bc0618338b71ff507bd50a6b948d48e98e
Daniil Gentili <daniil@daniil.it>
2021-12-24 00:25:01 +0100

    Phabel fix

--- src/danog/MadelineProto/Exception.php
+++ src/danog/MadelineProto/Exception.php
@@ -83,7 +83,7 @@ class Exception extends \Exception
      */
     public static function exceptionErrorHandler($errno = 0, $errstr = null, $errfile = null, $errline = null): bool
     {
-        $errfileReplaced = \preg_replace('/phabel\.transpiler\d+:/', '', $errfile ?? '');
+        $errfileReplaced = \preg_replace('/phabel-transpiler\d+\./', '', $errfile ?? '');
         // If error is suppressed with @, don't throw an exception
         if (\error_reporting() === 0
             || \strpos($errstr, 'headers already sent')
