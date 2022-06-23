8b76311eb71f431ca80de95c8147e433e3cb4726
Daniil Gentili <daniil@daniil.it>
2022-06-23 17:42:04 +0200

    Fix script

--- tests/db.php
+++ tests/db.php
@@ -1,7 +1,19 @@
 <?php
 
-
 // Switch to another database driver, for testing
+
+if (\getenv('ACTIONS_PHAR')) {
+    include 'madeline.php';
+    \copy('madelineBackup.php', 'madeline.php');
+} elseif (!\file_exists(__DIR__.'/../vendor/autoload.php') || \getenv('ACTIONS_FORCE_PREVIOUS')) {
+    echo 'You did not run composer update, using madeline.php'.PHP_EOL;
+    if (!\file_exists('madeline.php')) {
+        \copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+    }
+    include 'madeline.php';
+} else {
+    require_once 'vendor/autoload.php';
+}
 
 use danog\MadelineProto\Settings\Database\Memory;
 use danog\MadelineProto\Settings\Database\Mysql;
