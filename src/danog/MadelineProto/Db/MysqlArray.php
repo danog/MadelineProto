53662682c2685c9af07d9cd1b1f133ee8d752e7a
Alexander Pankratov <s.pankratoff@gmail.com>
2022-08-24 23:02:30 +0300

    Compress Mysql DB

--- src/danog/MadelineProto/Db/MysqlArray.php
+++ src/danog/MadelineProto/Db/MysqlArray.php
@@ -110,7 +110,7 @@ class MysqlArray extends SqlArray
                 `ts` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
                 PRIMARY KEY (`key`)
             )
-            ENGINE = InnoDB
+            ENGINE = InnoDB ROW_FORMAT=COMPRESSED
             CHARACTER SET 'utf8mb4' 
             COLLATE 'utf8mb4_general_ci'
         ");
