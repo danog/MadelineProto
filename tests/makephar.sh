eb48fe6da562b03f22e5277e82ecb0971321c821
Daniil Gentili <daniil@daniil.it>
2022-10-08 18:44:22 +0200

    CI build

--- tests/makephar.sh
+++ tests/makephar.sh
@@ -42,11 +42,11 @@ php8.0 $(which composer) update
 #php8.0 vendor/bin/phpunit
 php8.0 vendor/bin/phabel publish -d "$TAG"
 
-[ $php -eq 81 ] && SUBTAG=9999 || SUBTAG=9998
+[ $php -eq 81 ] && COMPOSER_TAG="$TAG" || COMPOSER_TAG="$TAG.9998"
 
 rm -rf vendor*
 git reset --hard
-git checkout "$TAG.$SUBTAG"
+git checkout "$COMPOSER_TAG"
 
 cd ..
 rm -rf phar
@@ -58,7 +58,7 @@ cd phar
 echo '{
     "name": "danog/madelineprotophar",
     "require": {
-        "danog/madelineproto": "'$TAG'.'$SUBTAG'"
+        "danog/madelineproto": "'$COMPOSER_TAG'"
     },
     "authors": [
         {
