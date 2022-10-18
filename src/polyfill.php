fc07cc74df87e11f7af1a02aed70335350446237
Daniil Gentili <daniil@daniil.it>
2022-10-18 14:01:55 +0200

    Update docs

--- src/polyfill.php
+++ src/polyfill.php
@@ -1,7 +1,7 @@
 <?php
 
 if (PHP_OS_FAMILY === 'Windows') {
-    echo(PHP_EOL.'========='.PHP_EOL.'WARNING: MadelineProto does not support Windows, please use Linux or another UNIX system (WSLv2 on Windows, Mac OS, BSD, etc).'.PHP_EOL.'========='.PHP_EOL.PHP_EOL);
+    //echo(PHP_EOL.'========='.PHP_EOL.'WARNING: MadelineProto does not support Windows, please use Linux or another UNIX system (WSLv2 on Windows, Mac OS, BSD, etc).'.PHP_EOL.'========='.PHP_EOL.PHP_EOL);
 }
 
 // Polyfill for some PHP 5 functions
