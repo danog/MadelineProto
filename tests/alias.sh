6fbe53b4468358b637e7f9037caebdacd7206cc6
Daniil Gentili <daniil@daniil.it>
2023-05-29 15:31:01 +0200

    Improve build system

--- tests/alias.sh
+++ tests/alias.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-docker login -p "$DUSER" -u "$DPASS"
+docker login -p "$dpass" -u "$duser"
 docker pull danog/madelineproto:next-debian
 docker tag danog/madelineproto:next-debian danog/madelineproto:next
 docker push danog/madelineproto:next
