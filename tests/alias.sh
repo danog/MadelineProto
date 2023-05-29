afaa2a3d9936e7fffe8ee19746a57fd304a114d3
Daniil Gentili <daniil@daniil.it>
2023-05-29 21:02:13 +0200

    Fixes

--- tests/alias.sh
+++ tests/alias.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
-exit 0
-docker login -p "$dpass" -u "$duser"
+
+docker login -p "$docker_password" -u "$docker_user"
 docker pull danog/madelineproto:next-debian
 docker tag danog/madelineproto:next-debian danog/madelineproto:next
 docker push danog/madelineproto:next
