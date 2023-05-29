9c7e0124391c2885a7bac7c5bc6426a0c1a84de8
Daniil Gentili <daniil@daniil.it>
2023-05-29 21:23:23 +0200

    Update builds

--- tests/alias.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-docker login -p "$docker_password" -u "$docker_user"
-docker pull danog/madelineproto:next-debian
-docker tag danog/madelineproto:next-debian danog/madelineproto:next
-docker push danog/madelineproto:next
