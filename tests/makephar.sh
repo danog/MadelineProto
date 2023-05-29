bffd34fb29e91390487385ab32447b787d433791
Daniil Gentili <daniil@daniil.it>
2023-05-29 14:25:32 +0200

    Unify build&deploy scripts

--- tests/makephar.sh
+++ tests/makephar.sh
@@ -181,9 +181,42 @@ input=$PWD
 
 cd "$madelinePath"
 
-if [ "$TAG" != "7777" ]; then
-    cp "$input/madeline$php$branch.phar" "madeline81.phar"
-    git remote add hub https://github.com/danog/MadelineProto
-    gh release upload "$TAG" "madeline81.phar"
-    rm "madeline81.phar"
+if [ "$TAG" == "7777" ]; then exit 0; fi
+
+cp "$input/madeline$php$branch.phar" "madeline81.phar"
+git remote add hub https://github.com/danog/MadelineProto
+gh release upload "$TAG" "madeline81.phar"
+rm "madeline81.phar"
+
+gh release edit --prerelease=false "$TAG"
+gh release edit --latest=true "$TAG"
+
+if [ "$DEPLOY_KEY" != "" ]; then
+    mkdir -p $HOME/.ssh
+    ssh-keyscan -t rsa github.com >> $HOME/.ssh/known_hosts
+    echo "$DEPLOY_KEY" > $HOME/.ssh/id_rsa
+    chmod 0600 $HOME/.ssh/id_rsa
 fi
+
+git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --global user.name "Github Actions"
+
+input=$PWD
+
+cd /tmp
+git clone git@github.com:danog/MadelineProtoPhar.git
+cd MadelineProtoPhar
+
+cp "$input/tools/phar.php" .
+for php in 81; do
+    echo -n "$COMMIT-$php" > release$php
+done
+
+git add -A
+git commit -am "Release $BRANCH - $COMMIT_MESSAGE"
+while :; do
+    git push origin master && break || {
+        git fetch
+        git rebase origin/master
+    }
+done
