c1df4739e7823aa306140cbc501e938da858e5bb
Daniil Gentili <daniil@daniil.it>
2023-05-30 23:57:23 +0200

    Prepare beta87

--- src/MTProto.php
+++ src/MTProto.php
@@ -139,7 +139,7 @@ final class MTProto implements TLCallback, LoggerGetter
      *
      * @var string
      */
-    const RELEASE = '8.0.0-beta86';
+    const RELEASE = '8.0.0-beta87';
     /**
      * We're not logged in.
      *
