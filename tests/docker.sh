8808fb594915a1caecaa447c54ad1b945f831ac7
Daniil Gentili <daniil@daniil.it>
2023-05-30 15:49:27 +0200

    Fixup

--- tests/docker.sh
+++ tests/docker.sh
@@ -5,11 +5,11 @@ docker login --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 docker buildx create --use --name wp --driver remote tcp://192.168.69.1:1234
 
 if ping -c 1 192.168.69.206; then
-	docker buildx create --use --name wp --driver remote tcp://192.168.69.206:1234
+	docker buildx create --append --name wp --driver remote tcp://192.168.69.206:1234
 fi
 
 if ping -c 1 192.168.69.236; then
-	docker buildx create --use --name wp --driver remote tcp://192.168.69.236:1234
+	docker buildx create --append --name wp --driver remote tcp://192.168.69.236:1234
 fi
 
 for f in debian alpine; do
