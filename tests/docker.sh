448fa0142fce3e79abecb928896e30f6ec5f85ed
Daniil Gentili <daniil@daniil.it>
2023-05-30 19:02:55 +0200

    Manual manifest builds

--- tests/docker.sh
+++ tests/docker.sh
@@ -19,23 +19,34 @@ for f in 192.168.69.236 192.168.69.207; do
 	fi
 done
 
-arch="linux/arm64"
+arches="arm64"
 if [ $has_x86 -eq 1 ]; then
-	arch="$arch,linux/amd64"
+	arches="$arches amd64"
 fi
 
 if [ $has_riscv -eq 1 ]; then
-	arch="$arch,linux/riscv64"
+	arches="$arches riscv64"
 fi
 
-echo "Building for $arch"
+echo "Building for $arches"
 
 for f in alpine debian; do
-	echo >> tests/dockerfiles/Dockerfile.$f
-	cat tests/dockerfiles/Dockerfile.$f | sed "s|FROM .*|FROM danog/php:8.2-fpm-$f|" >> tests/dockerfiles/Dockerfile.$f
+	manifest=""
+	for arch in $arches; do
+		cp tests/dockerfiles/Dockerfile.$f Dockerfile
+		if [ "$arch" == "riscv64" ]; then
+			sed "s|FROM .*|FROM danog/php:8.2-fpm-$f|" -i Dockerfile
+		fi
+		docker buildx build --platform linux/$arch . \
+			-t danog/madelineproto:$arch-next-$f \
+			--cache-from danog/madelineproto:next-$f \
+			--cache-to type=inline
+		
+		manifest="$manifest danog/madelineproto:$arch-next-$f"
+	done
 
-	docker buildx build -f tests/dockerfiles/Dockerfile.$f  --platform $arch . -t danog/madelineproto:next-$f --cache-from danog/madelineproto:next-$f --cache-to type=inline
-	docker push danog/madelineproto:next-$f
+	docker manifest create danog/madelineproto:next-$f $manifest
+	docker manifest push danog/madelineproto:next-$f
 
 	if [ "$CI_COMMIT_TAG" != "" ]; then
 		docker tag danog/madelineproto:next-$f danog/madelineproto:$f
