0c547b766340cd30f5ea1dfaf6a9dd561e5ceba5
Daniil Gentili <daniil@daniil.it>
2023-06-02 21:07:29 +0200

    Update RELEASE constant

--- src/MTProto.php
+++ src/MTProto.php
@@ -139,7 +139,7 @@ final class MTProto implements TLCallback, LoggerGetter
      *
      * @var string
      */
-    const RELEASE = '8.0.0-beta87';
+    const RELEASE = '8.0.0-beta88';
     /**
      * We're not logged in.
      *
