28a605c1f1e59e4af7157f24a62ae469843f6e9e
Daniil Gentili <daniil@daniil.it>
2023-06-05 16:53:42 +0200

    Update release number

--- src/MTProto.php
+++ src/MTProto.php
@@ -139,7 +139,7 @@ final class MTProto implements TLCallback, LoggerGetter
      *
      * @var string
      */
-    const RELEASE = '8.0.0-beta89';
+    const RELEASE = '8.0.0-beta90';
     /**
      * We're not logged in.
      *
