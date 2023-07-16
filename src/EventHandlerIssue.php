d181a234ba33a3559dfa3a33d09b6995c19f49b7
Daniil Gentili <daniil@daniil.it>
2023-07-16 14:44:49 +0200

    Add EventHandlerIssue class

---
+++ src/EventHandlerIssue.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace danog\MadelineProto;
+
+/**
+ * Represents an event handler issue.
+ */
+final class EventHandlerIssue
+{
+    public function __construct(
+        /** Issue message */
+        public readonly string $message,
+        /** Issue file */
+        public readonly string $file,
+        /** Issue line */
+        public readonly int $line,
+        /** Whether the issue is severe enough to block inclusion */
+        public readonly bool $severe,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return \sprintf(
+            Lang::$current_lang[$this->severe ? 'static_analysis_severe' : 'static_analysis_minor'],
+            "{$this->file}:{$this->line}",
+            $this->message
+        );
+    }
+
+    public function log(): void
+    {
+        Logger::log((string) $this, Logger::FATAL_ERROR);
+    }
+
+    public function getHTML(): string
+    {
+        $issueStr = \htmlentities((string) $this);
+        $color = $this->severe ? 'red' : 'yellow';
+        $warning = "<h2 style='color:$color;'>{$issueStr}</h2>";
+        return $warning;
+    }
+
+    public function throw(): void
+    {
+        throw new Exception(message: (string) $this, file: $this->file, line: $this->line);
+    }
+}
