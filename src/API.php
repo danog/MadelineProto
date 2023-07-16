72f71c1576e0e017c793456da9de4f2771f32e0c
Daniil Gentili <daniil@daniil.it>
2023-07-16 22:11:06 +0200

    beta 103

--- src/API.php
+++ src/API.php
@@ -52,7 +52,7 @@ final class API extends AbstractAPI
      *
      * @var string
      */
-    public const RELEASE = '8.0.0-beta102';
+    public const RELEASE = '8.0.0-beta103';
     /**
      * Secret chat was not found.
      *
