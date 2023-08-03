8df1c302401a04c38dde8a9fa56da58e3840cf92
Mahdi <mahdi.talaee1379@gmail.com>
2023-08-04 01:35:23 +0330

    Update InlineGameQuery.php

--- src/EventHandler/InlineGameQuery.php
+++ src/EventHandler/InlineGameQuery.php
@@ -4,4 +4,10 @@ namespace danog\MadelineProto\EventHandler;
 
 final class InlineGameQuery extends AbstractGameQuery
 {
+  
+    /** @internal */
+    public function __construct(MTProto $API, array $rawCallback)
+    {
+        parent::__construct($API, $rawCallback);
+    }
 }
