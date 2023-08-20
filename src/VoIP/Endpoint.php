5d93397f7ed7307a340fdda042b0eecbb7ac4a6a
Daniil Gentili <daniil@daniil.it>
2023-08-20 17:11:19 +0200

    Avoid issues during the handshake

--- src/VoIP/Endpoint.php
+++ src/VoIP/Endpoint.php
@@ -428,6 +428,6 @@ final class Endpoint
             'min_protocol' => VoIPController::MIN_PROTOCOL_VERSION,
             'audio_streams' => [VoIPController::CODEC_OPUS],
             'video_streams' => []
-        ], true));
+        ]));
     }
 }
