074db6c6babad351f55490ac43ecdc8caca71555
Daniil Gentili <daniil@daniil.it>
2023-08-20 22:36:05 +0200

    Increase release

--- src/API.php
+++ src/API.php
@@ -51,7 +51,7 @@ final class API extends AbstractAPI
      *
      * @var string
      */
-    public const RELEASE = '8.0.0-beta138';
+    public const RELEASE = '8.0.0-beta139';
     /**
      * Secret chat was not found.
      *
