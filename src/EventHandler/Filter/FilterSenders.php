4fe5a7f7de0afe1c18c8b378fe33fbe93588a70b
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-08-29 14:19:17 +0330

    Add FilterSenders

---
+++ src/EventHandler/Filter/FilterSenders.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Daniil Gentili <daniil@daniil.it>
+ * @copyright 2016-2023 Daniil Gentili <daniil@daniil.it>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+namespace danog\MadelineProto\EventHandler\Filter;
+
+use Attribute;
+use danog\MadelineProto\EventHandler;
+use danog\MadelineProto\EventHandler\AbstractMessage;
+use danog\MadelineProto\EventHandler\Query\ButtonQuery;
+use danog\MadelineProto\EventHandler\InlineQuery;
+use danog\MadelineProto\EventHandler\Update;
+
+/**
+ * Allow incoming or outgoing group messages made by a certain list of senders.
+ */
+#[Attribute(Attribute::TARGET_METHOD)]
+final class FilterSender extends Filter
+{
+    /** @var array<string|int> */
+    private readonly array $peers;
+    /** @var list<int> */
+    private readonly array $peersResolved;
+    public function __construct(string|int ...$idOrUsername)
+    {
+        $this->peers = \array_unique($idOrUsername);
+    }
+    public function initialize(EventHandler $API): Filter
+    {
+        if (\count($this->peers) === 1) {
+            return (new FilterSender(\array_values($this->peers)[0]))->initialize($API);
+        }
+        $res = [];
+        foreach ($this->peers as $peer) {
+            $res []= $API->getId($peer);
+        }
+        /** @psalm-suppress InaccessibleProperty */
+        $this->peersResolved = $res;
+        return $this;
+    }
+    public function apply(Update $update): bool
+    {
+        return ($update instanceof AbstractMessage && \in_array($update->senderId, $this->peersResolved, true)) ||
+            ($update instanceof ButtonQuery && \in_array($update->userId, $this->peersResolved, true)) || 
+            ($update instanceof InlineQuery && \in_array($update->userId, $this->peersResolved, true));
+    }
+}
