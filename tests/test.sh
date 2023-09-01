4290c569fd3852ce2c6c173027d2d331e3846e79
Daniil Gentili <daniil@daniil.it>
2023-09-01 14:56:43 +0200

    Add test.sh

---
+++ tests/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
+apk add procps git unzip github-cli openssh
+
+composer update
+composer build
+
