59824ac1190ba170178eb93db617a851153b6c07
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-09-05 18:19:42 +0330

    handle updateStatus

---
+++ src/EventHandler/User/Status/Emoji.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @copyright 2016-2023 Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+namespace danog\MadelineProto\EventHandler\User;
+
+use danog\MadelineProto\EventHandler\Update;
+use danog\MadelineProto\MTProto;
+
+/**
+ * The [emoji status](https://core.telegram.org/api/emoji-status) of a certain user has changed.
+ */
+final class Emoji extends Update
+{
+    /** @var int User identifier */
+    public readonly int $userId;
+
+    /** @var ?int [Custom emoji document ID](https://core.telegram.org/api/custom-emoji) */
+    public readonly ?int $emojiId;
+
+    /** @var ?int This status is valid until this date */
+    public readonly ?int $until;
+
+    public function __construct(MTProto $API, array $rawEmojiStatus)
+    {
+        parent::__construct($API);
+        $this->userId = $rawEmojiStatus['user_id'];
+        $this->emojiId = $rawEmojiStatus['emoji_status']['document_id'] ?? null;
+        $this->until = $rawEmojiStatus['emoji_status']['until'] ?? null;
+    }
+}
---
+++ src/EventHandler/User/Status/EmptyStatus.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @copyright 2016-2023 Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+namespace danog\MadelineProto\EventHandler\User\Status;
+
+use danog\MadelineProto\EventHandler\User\Status;
+
+/**
+ * User status has not been set yet.
+ */
+final class EmptyStatus extends Status
+{
+}
