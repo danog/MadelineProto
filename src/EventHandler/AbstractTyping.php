eb5a4085eeabcbb2c208c8f5873531b6cf90d3b4
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-09-05 18:19:09 +0330

    Improve Message action & handle Action updates

---
+++ src/EventHandler/AbstractTyping.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @copyright 2016-2023 Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+namespace danog\MadelineProto\EventHandler;
+
+use danog\MadelineProto\MTProto;
+
+/**
+ * A user is typing.
+ */
+final class AbstractTyping extends Update
+{
+    /** @var int The user id that is typing. */
+    public readonly int $userId;
+
+    /** @var AbstractAction Whether the user is typing, sending a media or doing something else. */
+    public readonly AbstractAction $action;
+
+    /** @internal */
+    public function __construct(MTProto $API, array $rawTyping)
+    {
+        parent::__construct($API);
+        $this->userId = $rawTyping['user_id'] ?? $API->getIdInternal($rawTyping['from_id']);
+        $this->action = AbstractAction::fromRawAction($rawTyping['action']);
+    }
+}
