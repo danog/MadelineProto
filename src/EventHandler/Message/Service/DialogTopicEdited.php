385e067e851d2d80650461658b8797501c95a612
Daniil Gentili <daniil@daniil.it>
2023-09-16 19:49:34 +0200

    Fix FromAdminOrOutgoing filter

--- src/EventHandler/Message/Service/DialogTopicEdited.php
+++ src/EventHandler/Message/Service/DialogTopicEdited.php
@@ -29,27 +29,27 @@ final class DialogTopicEdited extends ServiceMessage
         array $rawMessage,
         array $info,
 
-        /** 
-         * If not null, indicates that the topic name has changed, contains the new topic name. 
-         * 
+        /**
+         * If not null, indicates that the topic name has changed, contains the new topic name.
+         *
          * Ignore this field if null.
          */
         public readonly ?string $title,
         /**
          * If not null, indicates that the topic icon has changed, and contains the ID of the new [custom emoji](https://core.telegram.org/api/custom-emoji) used as topic icon (0 if it was removed).
-         * 
+         *
          * Ignore this field if null.
          */
         public readonly ?int $iconEmojiId,
         /**
-         * If not null, indicates whether the topic was opened or closed. 
-         * 
+         * If not null, indicates whether the topic was opened or closed.
+         *
          * Ignore this field if null.
          */
         public readonly ?bool $closed,
-        /** 
-         * If not null, indicates whether the topic was hidden or unhidden (only valid for the “General” topic, id=1). 
-         * 
+        /**
+         * If not null, indicates whether the topic was hidden or unhidden (only valid for the “General” topic, id=1).
+         *
          * Ignore this field if null.
          */
         public readonly ?bool $hidden
