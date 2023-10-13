bbf62c7e462d7b779ac07a44f19896d6ede8a089
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-10-13 12:56:56 +0330

    add new ChannelMessageViews update

---
+++ src/EventHandler/ChannelMessageViews.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @copyright 2016-2023 Amir Hossein Jafari <amirhosseinjafari8228@gmail.com>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+namespace danog\MadelineProto\EventHandler;
+
+use danog\MadelineProto\MTProto;
+use danog\MadelineProto\MTProtoTools\DialogId;
+
+/**
+ * Represents the view counter of a message in a channel has changed.
+ */
+final class ChannelMessageViews extends Update
+{
+    /** Channel ID */
+    public readonly int $chatId;
+
+    /** ID of the message */
+    public readonly int $id;
+
+    /** New view counter */
+    public readonly int $views;
+
+    /** @internal */
+    public function __construct(MTProto $API, array $rawDelete)
+    {
+        parent::__construct($API);
+        $this->chatId = DialogId::fromSupergroupOrChannel($rawDelete['channel_id']);
+        $this->id = $rawDelete['id'];
+        $this->views = $rawDelete['views'];
+    }
+}
