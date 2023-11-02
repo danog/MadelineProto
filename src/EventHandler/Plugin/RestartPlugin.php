ea67d826e2126af9dac46adbf16efaffb1aaacaa
َAhJ <AmirHosseinJafari8228@gmail.com>
2023-11-02 14:27:57 +0330

    Restore restart plugin

---
+++ src/EventHandler/Plugin/RestartPlugin.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of MadelineProto.
+ * MadelineProto is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * MadelineProto is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with MadelineProto.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author    Daniil Gentili <daniil@daniil.it>
+ * @copyright 2016-2023 Daniil Gentili <daniil@daniil.it>
+ * @license   https://opensource.org/licenses/AGPL-3.0 AGPLv3
+ * @link https://docs.madelineproto.xyz MadelineProto documentation
+ */
+
+namespace danog\MadelineProto\EventHandler\Plugin;
+
+use danog\MadelineProto\EventHandler\Filter\FilterCommand;
+use danog\MadelineProto\EventHandler\Message;
+use danog\MadelineProto\EventHandler\SimpleFilter\FromAdmin;
+use danog\MadelineProto\EventHandler\SimpleFilter\Incoming;
+use danog\MadelineProto\PluginEventHandler;
+
+/** 
+ * Plugin that offers a /restart command to admins that can be used to restart the bot, applying changes.
+ */
+final class RestartPlugin extends PluginEventHandler {
+    #[FilterCommand('restart')]
+    public function cmd(Incoming&Message&FromAdmin $_): void
+    {
+        $this->restart();
+    }
+}
