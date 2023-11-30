7310ca70e2151fb49f9bd1a8371d52d4d3de7207
Daniil Gentili <daniil@daniil.it>
2023-11-30 19:24:26 +0100

    Fixup

--- tests/test.sh
+++ tests/test.sh
@@ -4,27 +4,25 @@ set -ex
 
 export COMPOSER_PROCESS_TIMEOUT=100000
 
-touch /tmp/ci_status
+(
+    flock -x -w 100000 200 || exit 1
 
-exec 4</tmp/ci_status
-flock 4
+    touch /tmp/ci_status
+    if [ "$(cat /tmp/ci_status)" != "done" ]; then
+        apk add procps git unzip github-cli openssh
 
-if [ "$(cat /tmp/ci_status)" != "done" ]; then
-    apk add procps git unzip github-cli openssh
+        php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+        php composer-setup.php
+        php -r "unlink('composer-setup.php');"
+        mv composer.phar /usr/local/bin/composer
 
-    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-    php composer-setup.php
-    php -r "unlink('composer-setup.php');"
-    mv composer.phar /usr/local/bin/composer
+        php tests/jit.php
 
-    php tests/jit.php
+        composer update
 
-    composer update
-
-    echo done > /tmp/ci_status
-fi
-
-exec 4<&-
+        echo done > /tmp/ci_status
+    fi
+) 200>/var/lock/woodpecker.lock
 
 if [ "$1" == "cs" ]; then
     composer psalm
