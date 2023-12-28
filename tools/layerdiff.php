4e73503258ac57313a2b43f7d4a3c6825ac58fa6
Daniil Gentili <daniil@daniil.it>
2023-12-28 14:35:47 +0100

    Cleanup

--- tools/layerdiff.php
+++ tools/layerdiff.php
@@ -88,13 +88,13 @@ foreach (['methods', 'constructors'] as $type) {
             foreach ($final_new_args as $name => $ttype) {
                 if (!isset($final_old_args[$name]) && $name !== 'flags' && $name !== 'flags2') {
                     $name = Tools::markdownEscape($name);
-                    $res .= "Added $name param to $url\n";
+                    $res .= "- Added $name param to $url\n";
                 }
             }
             foreach ($final_old_args as $name => $ttype) {
                 if (!isset($final_new_args[$name]) && $name !== 'flags' && $name !== 'flags2') {
                     $name = Tools::markdownEscape($name);
-                    $res .= "Removed $name param from $url\n";
+                    $res .= "- Removed $name param from $url\n";
                 }
             }
         }
