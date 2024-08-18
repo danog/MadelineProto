d2aef813dcde00dd8b57b4b0fcecf7468799ca2d
Daniil Gentili <daniil@daniil.it>
2024-08-18 13:57:55 +0200

    Bump version for docker image

--- src/API.php
+++ src/API.php
@@ -51,7 +51,7 @@ final class API extends AbstractAPI
      *
      * @var string
      */
-    public const RELEASE = '8.2.2';
+    public const RELEASE = '8.2.3';
     /**
      * We're not logged in.
      *
