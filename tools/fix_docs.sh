399e4fbabcc5d227a0f821f8b6b40d511bae4667
Daniil Gentili <daniil@daniil.it>
2024-08-18 13:01:56 +0200

    Bump deps

--- tools/fix_docs.sh
+++ tools/fix_docs.sh
@@ -2,3 +2,5 @@
 
 find docs/ -name '*.md' -exec sed 's/\.md/\.html/g' -i {} +
 sed 's:(danog:(/PHP/danog:g' -i docs/docs/PHP/index.md
+
+sed 's/Array of \[long|string/Array of [long\\|string/g' docs/docs/API_docs/methods/* -i
