1eb71f25ca6a9b53f11f9c1bece9fb25105e2a9f
Daniil Gentili <daniil@daniil.it>
2024-10-21 15:56:38 +0000

    Bump to 8.3.5

--- src/API.php
+++ src/API.php
@@ -51,7 +51,7 @@ final class API extends AbstractAPI
      *
      * @var string
      */
-    public const RELEASE = '8.3.4';
+    public const RELEASE = '8.3.5';
     /**
      * We're not logged in.
      *
